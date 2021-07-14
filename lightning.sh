80d59591ce23cca0365f8f103bfe8a979640c2c3
MarkCodering <58692435+MarkCodering@users.noreply.github.com>
2021-07-14 14:29:43 +0800

    script update

---
+++ lightning.sh
@@ -0,0 +1,8 @@
+#Introduction
+echo "Welcome to use the video trimming automation script"
+
+#Initialisation
+echo "Script initialisation"
+echo "Linux apt and apt-get update"
+
+alias sudo apt-get update && apt update
