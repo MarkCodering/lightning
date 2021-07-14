a02a1b67578c4d026827308ce67e7c8428911473
MarkCodering <58692435+MarkCodering@users.noreply.github.com>
2021-07-14 15:05:50 +0800

    debugging

--- lightning.sh
+++ lightning.sh
@@ -11,31 +11,31 @@ echo ffmepg -version
 echo ffmepg -encoders
 
 #Trimming the vdieo
-echo "We are about to trim the video. Please make sure that the video is within this directory!\n"
-read -s -p "Please input your file name\n" orgFilename
+echo "We are about to trim the video. Please make sure that the video is within this directory!" 
+read -s -p "Please input your file name" orgFilename    
 
 ## Check if the file is within the same directory
-test -f ./$orgFilename && echo "$orgFilename exists.\n"
+test -f ./$orgFilename && echo "$orgFilename exists."
 
-read -s -p "Please type the file name for output\n" outFilename
+read -s -p "Please type the file name for output" outFilename
 
-read -s -p "Please the input the time you want to start: (00:00:00, hh:mm:ss)\n" startTime
+read -s -p "Please the input the time you want to start: (00:00:00, hh:mm:ss)" startTime
 ## Check the start time format
 if [[ $startTime =~ ^[0-9][0-9]:[0-9][0-9]:[0-9][0-9]$ ]]; then
-    echo "format is ok\n"
+    echo "format is ok"
 else
-    echo >&2 "format is wrong\n"
+    echo >&2 "format is wrong"
 fi
 
-read -s -p "Please type the file name for output\n" endTime
+read -s -p "Please type the file name for output" endTime
 ## Check the end time format
 if [[ $endTime =~ ^[0-9][0-9]:[0-9][0-9]:[0-9][0-9]$ ]]; then
-    echo "format is ok\n"
+    echo "format is ok"
 else
-    echo >&2 "format is wrong\n"
+    echo >&2 "format is wrong"
 fi
 
 ## Trimming the video
 echo sudo ffmepg -i $orgFilename --ss $orgFilename -codec copy -to $endTime $outFilename
 
-echo "Trimming completed!\n"
+echo "Trimming completed!"
