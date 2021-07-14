d00be0e1b2d4764b7e7c507a2eddff2f6acf44cc
MarkCodering <58692435+MarkCodering@users.noreply.github.com>
2021-07-14 14:38:34 +0800

    add echo and read to the script

--- lightning.sh
+++ lightning.sh
@@ -4,4 +4,5 @@ echo "Welcome to use the video trimming automation script"
 #Initialisation
 echo "Script initialisation"
 echo "Linux apt and apt-get update"
-sudo apt-get update && apt update
+read -s -p "Enter Password for sudo: " sudoPW
+echo $sudoPW | sudo apt-get update && sudo apt update
